291b3e2ca1a1775ff6ea3a5857a1231a5a282f48
anjiblob <anjiblob@gmail.com>
2022-09-24 21:17:09 -0400

    Fixing scrolling issue

--- class_scroll.py
+++ class_scroll.py
@@ -6,20 +6,31 @@ from pygame.math import Vector2
 
     
 class Scroll(object):
-    def __init__(self, position ,window_surface, screen_width, screen_height, state_stack, game_scene, stats):
+    def __init__(self, window_surface, screen_width, screen_height, state_stack, game_scene, stats):
         assets_dir = os.path.join("assets")
         sprites_dir = os.path.join(assets_dir, "sprites")
+
+        self.text_height = 50
+        self.text_default_height = 0
+        self.text_colour = (200,150,50)
+        self.max_scroll_height_top = 50
+        self.max_scroll_height_bottom = -200
+        self.stats = stats
+
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
 
         self.default_button = pygame.image.load(os.path.join(sprites_dir, "button", "button2.0.png"))
         self.new_default_button = pygame.transform.scale(self.default_button, (100,80))
 
         self.hovering_button = pygame.image.load(os.path.join(sprites_dir, "button", "buttonpress2.0.png"))
         self.new_hovering_button = pygame.transform.scale(self.hovering_button, (100,100))
-        self.position = Vector2(position)
+        
+        self.positon_cord = (self.screen_width*.5, self.text_height+ 300)
+        self.position = Vector2((self.positon_cord))
 
         
-        self.screen_width = screen_width
-        self.screen_height = screen_height
         
         self.state_stack = state_stack
         self.game_scene = game_scene
@@ -29,12 +40,7 @@ class Scroll(object):
         self.sub = window_surface.subsurface((120,0,300,150)).copy()
         self.sub.fill((200,150,50))
         
-        self.text_height = 50
-        self.text_default_height = 0
-        self.text_colour = (200,150,50)
-        self.max_scroll_height_top = 50
-        self.max_scroll_height_bottom = -200
-        self.stats = stats
+        
         
         self.background_surface = pygame.image.load(os.path.join(sprites_dir, "pause", "scrolling_bg.png"))
         self.background = pygame.transform.scale(self.background_surface, (400,250))
@@ -63,7 +69,7 @@ class Scroll(object):
         
         self.button_hover = False
         
-        self.rect = self.Quest_panal.get_rect(center=position)
+        self.rect = self.Quest_panal.get_rect(center=(self.positon_cord))
         self.hitbox = self.rect.inflate(0, 0)
         
         
@@ -75,10 +81,11 @@ class Scroll(object):
         self.hitbox.center = self.position
         
         
-        self.window_surface.blit(self.Quest_panal, (155,250))
-        pygame.draw.rect(self.window_surface, (0, 230, 0), self.rect, 2)
-        pygame.draw.rect(self.window_surface, (250, 30, 0), self.hitbox, 2)
-        if self.hitbox.collidepoint(pos):
+        
+        #pygame.draw.rect(self.window_surface, (0, 230, 0), self.rect, 2)
+        #pygame.draw.rect(self.window_surface, (250, 30, 0), self.hitbox, 2)
+        
+        if self.rect.collidepoint(pos):
             self.current_image = self.hovering_image
             
             if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
@@ -118,24 +125,24 @@ class Scroll(object):
                 if e.button == 4:
                     if self.text_height == self.max_scroll_height_top:
                         self.text_height += 0
-                        self.rect.y += 0
+                        self.position.y += 0
                     elif self.text_height == self.max_scroll_height_bottom:
                         self.text_height += 10
-                        self.rect.y += 10
+                        self.position.y += 10
                     else:
                         self.text_height += 10
-                        self.rect.y += 10
+                        self.position.y += 10
                 if e.button == 5:
                     if self.text_height == self.max_scroll_height_top:
                         self.text_height -= 10
-                        self.rect.y -= 10
+                        self.position.y -= 10
             
                     elif self.text_height <= self.max_scroll_height_bottom:
                         self.text_height -= 0
-                        self.rect.y -= 0
+                        self.position.y -= 0
                     else:
                         self.text_height -= 10
-                        self.rect.y -= 10
+                        self.position.y -= 10
            
                
             ########################### // Scrolling boundries // ##############################
@@ -164,7 +171,7 @@ class Scroll(object):
         #self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.1))
         
         #self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.15))
-        self.sub.blit(self.Quest_panal, (self.rect.x, self.rect.y))
+        self.sub.blit(self.Quest_panal, (self.position.x, self.position.y))
         self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.2))
         self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.25))
             
