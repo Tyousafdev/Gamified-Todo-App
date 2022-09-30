4f38f29893c5b60fe6b77773dc42feea55e1e071
anjiblob <anjiblob@gmail.com>
2022-09-30 02:36:05 -0400

    Fixed scrolling logic (old version/too messy)

--- class_scroll.py
+++ class_scroll.py
@@ -1,5 +1,5 @@
 
-from re import L
+
 import pygame,os,sys
 from pygame.math import Vector2
         
@@ -13,8 +13,12 @@ class Scroll(object):
         self.text_height = 50
         self.text_default_height = 0
         self.text_colour = (200,150,50)
-        self.max_scroll_height_top = 50
-        self.max_scroll_height_bottom = -200
+
+
+        self.max_scroll_height_top = 270
+        self.max_scroll_height_bottom = 100
+
+
         self.stats = stats
 
         self.screen_width = screen_width
@@ -27,7 +31,7 @@ class Scroll(object):
         self.hovering_button = pygame.image.load(os.path.join(sprites_dir, "button", "buttonpress2.0.png"))
         self.new_hovering_button = pygame.transform.scale(self.hovering_button, (100,100))
         
-        self.positon_cord = (self.screen_width*.5, self.text_height+ 300)
+        self.positon_cord = (self.screen_width*.5, 270)
         self.position = Vector2((self.positon_cord))
 
         
@@ -37,8 +41,8 @@ class Scroll(object):
         
         self.font = pygame.font.SysFont('Consolas', 20)
         
-        self.sub = window_surface.subsurface((120,0,300,150)).copy()
-        self.sub.fill((200,150,50))
+        #self.sub = window_surface.subsurface((120,0,300,150)).copy()
+        #self.sub.fill((200,150,50))
         
         
         
@@ -71,50 +75,32 @@ class Scroll(object):
         
         self.rect = self.Quest_panal.get_rect(center=(self.positon_cord))
         self.hitbox = self.rect.inflate(0, 0)
+        self.display_button = True
+        self.quest_box_size = (300,150)
+        self.quest_box_surface = pygame.Surface((self.quest_box_size))
+        self.quest_box_rect = self.quest_box_surface.get_rect()
+        self.quest_box_rect.x = 150
+        self.quest_box_rect.y = 250
+        self.quest_box_surface.fill((255,255,255))
         
-        
-
-    def update_button(self):
-        pos = pygame.mouse.get_pos()
-        action = False
-        self.rect.center = self.position
-        self.hitbox.center = self.position
-        
-        
-        
-        #pygame.draw.rect(self.window_surface, (0, 230, 0), self.rect, 2)
-        #pygame.draw.rect(self.window_surface, (250, 30, 0), self.hitbox, 2)
-        
-        if self.rect.collidepoint(pos):
-            self.current_image = self.hovering_image
-            
-            if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
-                #button_sound.play()
-                self.click = True
-                action = True
-                
-            if pygame.mouse.get_pressed()[0] == 0:
-                self.click = False
-                
-
-            if self.hitbox.collidepoint(pos) == True and self.button_hover == False:
-                #button_hover_sound.play()
-                self.button_hover = True
     
 
-
-        else:
-            self.current_image = self.default_image
-            self.button_hover = False
-        return action    
+       
+          
         
     def update_event(self, window_surface):
-        
-        self.draw_text(window_surface)
+        self.window_surface.blit(self.quest_box_surface, (self.quest_box_rect))
+        #self.draw_text(window_surface)
+    
+        if self.update_button(0):
+            print("button 1 working")
 
-        if self.update_button():
-            
-            print("workings")
+        if self.update_button(50):
+            print("button 2 working")
+
+        if self.update_button(100):
+            print("button 3 working")
+
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 pygame.quit()
@@ -123,28 +109,38 @@ class Scroll(object):
             ########################### // Scrolling boundries // ##############################
             if e.type == pygame.MOUSEBUTTONUP:
                 if e.button == 4:
-                    if self.text_height == self.max_scroll_height_top:
-                        self.text_height += 0
+
+                    if self.position.y  == self.max_scroll_height_top:
                         self.position.y += 0
-                    elif self.text_height == self.max_scroll_height_bottom:
-                        self.text_height += 10
+                
+                    elif self.position.y  == self.max_scroll_height_bottom:
                         self.position.y += 10
+                    
                     else:
-                        self.text_height += 10
+                        
                         self.position.y += 10
+                        
+                    
+
+
+
                 if e.button == 5:
-                    if self.text_height == self.max_scroll_height_top:
-                        self.text_height -= 10
+                  
+                    if self.position.y  == self.max_scroll_height_top:
+                        
                         self.position.y -= 10
+                        
             
-                    elif self.text_height <= self.max_scroll_height_bottom:
-                        self.text_height -= 0
+                    elif self.position.y == self.max_scroll_height_bottom:
                         self.position.y -= 0
+                    
                     else:
-                        self.text_height -= 10
                         self.position.y -= 10
-           
-               
+                    
+                        
+                
+                    
+            
             ########################### // Scrolling boundries // ##############################
             
             
@@ -160,20 +156,48 @@ class Scroll(object):
                     #game_sound.play(-1)
                     self.state_stack[self.game_scene()]
                        
-        #pygame.display.update()           
-    def draw_text(self, window_surface):
+        #pygame.display.update()  
+    def update_button(self, pos_y):
+        self.rect.center = self.position 
+        self.hitbox.center = self.position 
+        
+        self.rect.y = self.rect.y + pos_y
+        self.hitbox.y = self.hitbox.y + pos_y
+       
         
         
-        self.sub.blit(self.background, (0,0))
         
-        #self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height))
-        #self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.05))
-        #self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.1))
-        
-        #self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.15))
-        self.sub.blit(self.Quest_panal, (self.position.x, self.position.y))
-        self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.2))
-        self.sub.blit(self.Quest_panal,  (self.screen_width*.03,self.text_height+ self.screen_height*.25))
+        if self.display_button:
+            pos = pygame.mouse.get_pos()
+            action = False
             
-        window_surface.blit(self.sub, (155,250))
-        window_surface.blit(self.frame_img, (155,250))
+            
+            #pygame.draw.rect(self.window_surface, (0, 230, 0), self.rect, 2)
+            #pygame.draw.rect(self.window_surface, (250, 30, 0), self.hitbox, 2)
+            self.window_surface.blit(self.Quest_panal, (self.rect.x, self.rect.y))
+            
+            if self.hitbox.collidepoint(pos):
+                self.current_image = self.hovering_image
+                
+                if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
+                    #button_sound.play()
+                    self.click = True
+                    action = True
+                    
+                if pygame.mouse.get_pressed()[0] == 0:
+                    self.click = False
+                    
+
+                if self.hitbox.collidepoint(pos) == True and self.button_hover == False:
+                    #button_hover_sound.play()
+                    self.button_hover = True
+        
+
+
+            else:
+                self.current_image = self.default_image
+                self.button_hover = False
+            return action 
+
+        
+  
